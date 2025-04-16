9a8b5c2d42510880bd1076c7fcc3ea6eaa9bf943
Arish <arish@gmail.com>
2025-04-16 20:24:09 +0530

    change the name of website

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -43,7 +43,7 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">
-              <BsShopWindow /> Ecommerce App
+              <BsShopWindow /> e-Dukan
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <SearchInput />

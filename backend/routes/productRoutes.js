8c61a92b6f03ea594143ae4b74ba37f34d75b032
Arish <arish@gmail.com>
2025-04-23 10:33:23 +0530

    fix rote

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,6 +7,7 @@ import {
   productCategoryController,
   productCountController,
   productFilterController,
+  productListController,
   productPhotoController,
   relatedProductController,
   searchProductController,

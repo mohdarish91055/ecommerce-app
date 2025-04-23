4b7308f98c2c0ef2772f14db09a65fb6f779f24d
Arish <arish@gmail.com>
2025-04-23 10:41:48 +0530

    fix rote

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -227,8 +227,8 @@ export const productCountController = async (req, res) => {
 // //product list base on page
 export const productListController = async (req, res) => {
   try {
-    const perPage = 3;
-    const page = req.params.page ? req.params.page : 1;
+    const perPage = 6;
+    const page = parseInt(req.params.page) || 1;
     const products = await productModel
       .find({})
       .select("-photo")

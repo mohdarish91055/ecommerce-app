9a09ed26f2604784b34e259dc7c376f5c8109f16
Arish <arish@gmail.com>
2025-04-24 15:24:39 +0530

    add new url on cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ import cartRoute from "./routes/cartRoute.js";
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://ecommerce-app-1-7obg.onrender.com",
+    ],
     credentials: true,
   })
 );
